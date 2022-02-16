dd1368f2987ee78871b690bf4b6a504a929a5790
bayupb <kampuspb.bayu@gmail.com>
2022-02-16 13:33:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  content: [
+    '*'
+  ],
+  theme: {
+    fontFamily: {
+      'quicksand': ['Quicksand', 'sans-serif']
+    },
+    extend: {},
+  },
+   plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
+}
